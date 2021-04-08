2534899e8d044e0c7e338315305ad56234676182
unknown <ibmjavanh09@iiht.tech>
2021-04-08 11:33:30 +0530

    first commit

---
+++ src/main/java/com/example/mysqldemo/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.mysqldemo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+
+@Entity
+
+public class User {
+	@GeneratedValue()
+	@Id
+	private Integer id;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+}
